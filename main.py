12d2f83cbf6b509a468e3bd0c122ef1faca1512d
DanyHidalgo <danielhl@ufm.edu>
2022-04-27 18:24:28 -0600

    update_board updated

--- main.py
+++ main.py
@@ -18,9 +18,45 @@ def print_board(board):
     print('\n'.join([str(row) for row in board]))
 
 
-def update_board(board, position, player):
+def update_board(board, pos, player):
     # TODO: modificar tablero con nuevo movimiento del jugador en posicion indicada.
-    pass
+
+    if pos == 1:
+        board[0][0] = player
+        return board
+    
+    if pos == 2:
+        board[0][1] = player
+        return board
+    
+    if pos == 3:
+        board[0][2] = player
+        return board
+    
+    if pos == 4:
+        board[1][0] = player
+        return board
+
+    if pos == 5:
+        board[1][1] = player
+        return board
+    
+    if pos == 6:
+        board[1][2] = player
+        return board
+    
+    if pos == 7:
+        board[2][0] = player
+        return board
+
+    if pos == 8:
+        board[2][1] = player
+        return board
+    
+    if pos == 9:
+        board[2][2] = player
+        return board
+
 
 
 def check_for_winner(board, player):
@@ -31,5 +67,9 @@ def check_for_winner(board, player):
 '''
 Testing: 
 '''
+
+
 board = create_empty_board()
+for i in range(1, len(board()):
+    update_board(board, i, "X")
 print_board(board)
